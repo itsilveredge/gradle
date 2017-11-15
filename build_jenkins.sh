f2162f22865608feb051289894370cffa6c94257
Prateek Sharma <prateek.sharma@gaussnetworks.com>
2017-11-15 19:37:57 +0530

    submodules deleted

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -1,21 +1,6 @@
 echo $WORKSPACE
 
 cd $WORKSPACE/poker
-if [ ! -f html5poker.ngpoker/.git ]; then
-    echo "Initializing ngpoker project"
-    git submodule update --init -- html5poker.ngpoker
-fi
-
-if [ ! -f taaskingclient.node/.git ]; then
-    echo "Initializing node project"
-    git submodule update --init -- taaskingclient.node
-fi
-
-if [ ! -f taaskingclient.nodeTourneyPanel/.git ]; then
-    echo "Initializing tourney panel project"
-    git submodule update --init -- taaskingclient.nodeTourneyPanel
-fi
-
 if [ ! -f taaskingclient.server/.git ]; then
     echo "Initializing server project"
     git submodule update --init -- taaskingclient.server
@@ -24,11 +9,6 @@ fi
 if [ ! -f taaskingclient.userDB/.git ]; then
     echo "Initializing userdb project"
     git submodule update --init -- taaskingclient.userDB
-fi
-
-if [ ! -f taaskingclient.videoreplay/.git ]; then
-    echo "Initializing video replay project"
-    git submodule update --init -- taaskingclient.videoreplay
 fi
 
 echo "Resetting all the submodules"
@@ -40,18 +20,6 @@ git submodule foreach git reset --hard HEAD
 echo "Fetching new changes in each submodule"
 git submodule foreach git fetch --all --prune
 
-cd $WORKSPACE/poker/html5poker.ngpoker
-git checkout $ngpoker_branch
-git pull origin $ngpoker_branch
-
-cd $WORKSPACE/poker/taaskingclient.node
-git checkout $node_branch
-git pull origin $node_branch
-
-cd $WORKSPACE/poker/taaskingclient.nodeTourneyPanel
-git checkout $tourneyPanel_branch
-git pull origin $tourneyPanel_branch
-
 cd $WORKSPACE/poker/taaskingclient.server
 git checkout $server_branch
 git pull origin $server_branch
@@ -59,10 +27,6 @@ git pull origin $server_branch
 cd $WORKSPACE/poker/taaskingclient.userDB
 git checkout $userDB_branch
 git pull origin $userDB_branch
-
-cd $WORKSPACE/poker/taaskingclient.videoreplay
-git checkout $videoreplay_branch
-git pull origin $videoreplay_branch
 
 cd $WORKSPACE/poker/
 
@@ -73,10 +37,3 @@ sed -i 's,localhost,$dev_ip,g' taaskingclient.server/TaashkingCommon/src/hiberna
 
 sed -i 's,root,gauss,g' taaskingclient.userDB/UserCommon/src/hibernate2.cfg.xml
 sed -i 's,localhost,$dev_ip,g' taaskingclient.userDB/UserCommon/src/hibernate2.cfg.xml
-
-echo "Changing the mysql properties file in video replay"
-sed -i 's,root,gauss,g' taaskingclient.videoreplay/WebContent/WEB-INF/classes/MySql.properties
-
-echo "Building ngpoker"
-cd html5poker.ngpoker
-grunt --force build
--- poker_build.sh
+++ poker_build.sh
@@ -1,37 +1,14 @@
 #!/bin/bash
 # ./poker_build.sh ngpoker node tp server userdb replay devip
-ngpoker_branch=$1
-node_branch=$2
-tourneyPanel_branch=$3
 server_branch=$4
 userDB_branch=$5
-videoreplay_branch=$6
 dev_ip=$7
 
-echo "ngpoker $ngpoker_branch"
-echo "node $node_branch"
-echo "tp $tourneyPanel_branch"
 echo "server $server_branch"
 echo "userdb $userDB_branch"
-echo "replay $videoreplay_branch"
 echo "dev_ip $dev_ip"
 
 echo `pwd`
-
-if [ ! -f html5poker.ngpoker/.git ]; then
-    echo "Initializing ngpoker project"
-    git submodule update --init -- html5poker.ngpoker
-fi
-
-if [ ! -f taaskingclient.node/.git ]; then
-    echo "Initializing node project"
-    git submodule update --init -- taaskingclient.node
-fi
-
-if [ ! -f taaskingclient.nodeTourneyPanel/.git ]; then
-    echo "Initializing tourney panel project"
-    git submodule update --init -- taaskingclient.nodeTourneyPanel
-fi
 
 if [ ! -f taaskingclient.server/.git ]; then
     echo "Initializing server project"
@@ -43,11 +20,6 @@ if [ ! -f taaskingclient.userDB/.git ]; then
     git submodule update --init -- taaskingclient.userDB
 fi
 
-if [ ! -f taaskingclient.videoreplay/.git ]; then
-    echo "Initializing video replay project"
-    git submodule update --init -- taaskingclient.videoreplay
-fi
-
 echo "Resetting all the submodules"
 git submodule foreach git add -A
 
@@ -56,21 +28,6 @@ git submodule foreach git reset --hard HEAD
 
 echo "Fetching new changes in each submodule"
 git submodule foreach git fetch --all --prune
-
-cd html5poker.ngpoker
-git checkout $ngpoker_branch
-git pull origin $ngpoker_branch
-cd ..
-
-cd taaskingclient.node
-git checkout $node_branch
-git pull origin $node_branch
-cd ..
-
-cd taaskingclient.nodeTourneyPanel
-git checkout $tourneyPanel_branch
-git pull origin $tourneyPanel_branch
-cd ..
 
 cd taaskingclient.server
 git checkout $server_branch
@@ -82,11 +39,6 @@ git checkout $userDB_branch
 git pull origin $userDB_branch
 cd ..
 
-cd taaskingclient.videoreplay
-git checkout $videoreplay_branch
-git pull origin $videoreplay_branch
-cd ..
-
 echo "Changing the username in hibernate files"
 
 sed -i "s,root,gauss,g" taaskingclient.server/TaashkingCommon/src/hibernate.cfg.xml
@@ -95,16 +47,7 @@ sed -i "s,localhost,$dev_ip,g" taaskingclient.server/TaashkingCommon/src/hiberna
 sed -i "s,root,gauss,g" taaskingclient.userDB/UserCommon/src/hibernate2.cfg.xml
 sed -i "s,localhost,$dev_ip,g" taaskingclient.userDB/UserCommon/src/hibernate2.cfg.xml
 
-echo "Building ngpoker"
-cd html5poker.ngpoker
-grunt --force build
-cd ..
-
 echo "Building server"
 gradle -Ddev_ip=$dev_ip clean build -x test
-
-echo "Building replay"
-cd taaskingclient.videoreplay
-mvn clean install
 
 cd ..
