2bbed3eaad48393967ade5601b4993e82c7a359d
Prateek Sharma <prateek.sharma@gaussnetworks.com>
2018-07-02 12:30:00 +0530

    updating jenkins build steps

--- build_jenkins.sh
+++ build_jenkins.sh
@@ -1,6 +1,5 @@
-echo $WORKSPACE
-
 cd $WORKSPACE/poker
+
 if [ ! -f taaskingclient.server/.git ]; then
     echo "Initializing server project"
     git submodule update --init -- taaskingclient.server
@@ -29,11 +28,35 @@ git checkout $userDB_branch
 git pull origin $userDB_branch
 
 cd $WORKSPACE/poker/
-
 echo "Changing the username in hibernate files"
+sed -i "s,root,gauss,g" taaskingclient.server/TaashkingCommon/src/hibernate.cfg.xml
+sed -i "s,root,gauss,g" taaskingclient.userDB/UserCommon/src/hibernate2.cfg.xml
+sed -i "s,localhost,$dev_ip,g" taaskingclient.server/TaashkingCommon/src/hibernate.cfg.xml
+sed -i "s,localhost,$dev_ip,g" taaskingclient.userDB/UserCommon/src/hibernate2.cfg.xml
 
-sed -i 's,root,gauss,g' taaskingclient.server/TaashkingCommon/src/hibernate.cfg.xml
-sed -i 's,localhost,$dev_ip,g' taaskingclient.server/TaashkingCommon/src/hibernate.cfg.xml
+cd $WORKSPACE/poker/taaskingclient.server/AlterScripts
+for i in `ls -R current/ | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }'| grep sql`
+do
+    echo "file=$i"
+    mysql -ugauss -h$dev_ip < $i
+done
 
-sed -i 's,root,gauss,g' taaskingclient.userDB/UserCommon/src/hibernate2.cfg.xml
-sed -i 's,localhost,$dev_ip,g' taaskingclient.userDB/UserCommon/src/hibernate2.cfg.xml
+cd $WORKSPACE/tourney_panel/db/
+for i in `ls -R current/ | awk '/:$/&&f{s=$0;f=0}/:$/&&!f{sub(/:$/,"");s=$0;f=1;next}NF&&f{ print s"/"$0 }'| grep sql`
+do
+    echo "file=$i"
+    mysql -ugauss -h$dev_ip < $i
+done
+
+cd $WORKSPACE/replay
+/opt/maven/bin/mvn clean install
+
+cd $WORKSPACE/ngPoker
+npm install
+grunt --force build
+
+cd $WORKSPACE/adda52analytics
+git reset --hard HEAD
+git fetch --all --prune
+git checkout $nodeAnalytics
+git pull origin $nodeAnalytics
